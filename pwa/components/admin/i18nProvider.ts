cb7e82ee936b208dbf79cfe28c261545c2484133
ed <sarvarov.edward@e-queo.com>
2026-01-15 13:32:13 +0300

    русская локаль. Убрал кнопку создания заявки в админке

--- pwa/components/admin/i18nProvider.ts
+++ pwa/components/admin/i18nProvider.ts
@@ -2,15 +2,17 @@ import {resolveBrowserLocale} from "react-admin";
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import englishMessages from "ra-language-english";
 import frenchMessages from "ra-language-french";
+import russianMessages from "ra-language-russian";
 
 const messages = {
   fr: frenchMessages,
   en: englishMessages,
+  ru: russianMessages,
 };
 const i18nProvider = polyglotI18nProvider(
   // @ts-ignore
-  (locale) => (messages[locale] ? messages[locale] : messages.en),
-  resolveBrowserLocale()
+  (locale) => (messages[locale] ? messages[locale] : messages.ru),
+  "ru"
 );
 
 export default i18nProvider;
